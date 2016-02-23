be63214bf2d06e58aeadc541dc8105bf6f1ac6ee
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2016-02-23 18:50:45 +0100

    added Java Concurrency In Practice fodler, create basic mvn project

---
+++ Java Concurrency In Practice/src/main/java/com/petlew/lyfe/App.java
@@ -0,0 +1,13 @@
+package com.petlew.lyfe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
