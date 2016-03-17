6b8c37cb84a471ad07502f1ddda3521b84d84d28
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2016-03-17 06:12:14 +0100

    hello world play

--- Scala/tradenav/app/controllers/Application.scala
+++ Scala/tradenav/app/controllers/Application.scala
@@ -1,7 +1,15 @@
 package controllers
 
+import javax.inject.Inject
+import javax.inject.Singleton
 import play.api.mvc.Controller
+import play.api.mvc.Action
 
-object Application extends Controller {
-  Ok(views.html.index("Your new application is ready!"))
+
+@Singleton
+class ApplicationController @Inject() extends Controller {
+  def index = Action {
+    Ok(views.html index("Your new application is COOL."))
+  }
+
 }
