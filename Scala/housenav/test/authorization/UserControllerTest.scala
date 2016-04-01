44ca231e103687c11356c2aa93f4b81a99b8392d
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2016-04-01 07:33:07 +0200

    not working test

--- Scala/housenav/test/authorization/UserControllerTest.scala
+++ Scala/housenav/test/authorization/UserControllerTest.scala
@@ -21,75 +21,79 @@ import org.scalacheck.Arbitrary._
 
 class UserControllerTest extends PlaySpec with PropertyChecks with OneAppPerTest {
 
-    "UserController" should {
+  "UserController" should {
 
-        val userRoot: String = "/user"
+    val userRoot: String = "/user"
 
-        "render new user page" in {
-            val userHome: Future[Result] = route(app, FakeRequest(GET, userRoot + "/new")) get
+    "render new user page" in {
+      val userHome: Future[Result] = route(app, FakeRequest(GET, userRoot + "/new")) get
 
-            status(userHome) mustBe OK
-            contentType(userHome) mustBe Some("text/html")
-            contentAsString(userHome) must include("email")
-            contentAsString(userHome) must include("name")
-            contentAsString(userHome) must include("password")
-        }
+      status(userHome) mustBe OK
+      contentType(userHome) mustBe Some("text/html")
+      contentAsString(userHome) must include("email")
+      contentAsString(userHome) must include("name")
+      contentAsString(userHome) must include("password")
+    }
 
-        def usersDAO(implicit app: Application) = {
-            val app2UsersDAO = Application.instanceCache[UsersDAO]
-            app2UsersDAO(app)
-        }
+    def usersDAO(implicit app: Application) = {
+      val app2UsersDAO = Application.instanceCache[UsersDAO]
+      app2UsersDAO(app)
+    }
 
-        "should be able to create new user" in {
-            val alphaNumerics = ('!' to ')') ++ ('a' to 'z') ++ ('0' to '9') ++ ('A' to 'Z')
-            val gene = Gen.listOf(alphaNumerics)
-            
-            val properPasswords: Gen[String] = 
-                (Gen.choose(6, 20) flatMap {n => Gen.listOfN(n, alphaNumerics).flatMap { e =>  e.mkString}})
-            val properEmails: Gen[String] = arbitrary[String]
-                .filter(_.length > 0)
-                .flatMap(_ + "@test.com")
-            val properNames: Gen[String] = arbitrary[String]
-                .filter(e => e.isEmpty || e.length > 2 && e.length < 21)
-            forAll(properPasswords, properEmails, properNames) { (password: String, email: String, name: String) =>
-                val properJson: JsValue = Json.parse(s"""{"name": "$name", "password": "$password", "email":"$email"}""")
-                val createUser: Future[Result] = route(app, FakeRequest(POST, userRoot + "/new").withJsonBody(properJson)) get
+    "should be able to create new user" in {
+      val specialChars: Seq[Char] = Array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')')
+      val properPasswords: Gen[String] = for {
+        n <- Gen.choose(2, 5)
+        specials <- Gen.listOfN(n, specialChars)
+        nums <- Gen.listOfN(n, Gen.numChar)
+        lowerCase <- Gen.listOfN(n, Gen.alphaLowerChar)
+        upperCase <- Gen.listOfN(n, Gen.alphaUpperChar)
+      } yield specials.mkString ++ nums.mkString ++ lowerCase.mkString ++ upperCase.mkString
+      
+      val properEmails: Gen[String] = Gen.alphaStr
+        .filter(_.length > 0)
+        .flatMap(_ + "@test.com")
+      val properNames: Gen[String] = Gen.alphaStr
+        .suchThat(e => e.isEmpty || e.length > 2 && e.length < 21)
+      forAll(properPasswords, properEmails, properNames) { (password: String, email: String, name: String) =>
+        val properJson: JsValue = Json.parse(s"""{"name": "$name", "password": "$password", "email":"$email"}""")
+        val createUser: Future[Result] = route(app, FakeRequest(POST, userRoot + "/new").withJsonBody(properJson)) get
 
-                status(createUser) mustBe SEE_OTHER
-                redirectLocation(createUser) mustBe Some("/")
-                flash(createUser).get("success") mustBe Some("You have created account")
+        status(createUser) mustBe SEE_OTHER
+        redirectLocation(createUser) mustBe Some("/")
+        flash(createUser).get("success") mustBe Some("You have created account")
 
-                val getFirstUserByEmailFromSeq = (seq: Seq[User]) =>
-                    seq.filter(_.email == email).head
+        val getFirstUserByEmailFromSeq = (seq: Seq[User]) =>
+          seq.filter(_.email == email).head
 
-                val createdUser: User = Await.result(usersDAO.getAll.map(getFirstUserByEmailFromSeq), Duration.Inf)
+        val createdUser: User = Await.result(usersDAO.getAll.map(getFirstUserByEmailFromSeq), Duration.Inf)
 
-                createdUser.email mustBe email
-                createdUser.name mustBe (if (name.isEmpty) None else Some(name))
-            }
-
-        }
-
-        "should validate fields" in {
-            fail
-        }
-
-        "should be able to log in" in {
-            fail
-        }
-
-        "should be able to update data when logged in" in {
-            fail
-        }
-
-        "should be able to log out" in {
-            fail
-        }
-
-        "should be able to delete his account" in {
-            fail
-        }
+        createdUser.email mustBe email
+        createdUser.name mustBe (if (name.isEmpty) None else Some(name))
+      }
 
     }
+
+    "should validate fields" in {
+      fail
+    }
+
+    "should be able to log in" in {
+      fail
+    }
+
+    "should be able to update data when logged in" in {
+      fail
+    }
+
+    "should be able to log out" in {
+      fail
+    }
+
+    "should be able to delete his account" in {
+      fail
+    }
+
+  }
 
 }
