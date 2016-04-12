af10a6da734e6175a0afc895afbe213eed096bbb
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2016-04-12 07:03:19 +0200

    super alpha version, quite working

--- Haskell/RPS/Main.hs
+++ Haskell/RPS/Main.hs
@@ -2,13 +2,12 @@
 
 module Main where
 
+import           Data.List
+import           Data.Map.Strict
 import           Data.Maybe
 import           Data.Random.Extras
 import           Data.Random.Source.DevRandom
 import           Data.RVar
-import qualified Data.Text as T
-import Data.List
-import Data.Map.Strict
 
 data Hand = Rock | Scissors | Paper deriving (Eq, Show)
 instance Ord Hand where
@@ -34,9 +33,9 @@ playAgainst userHand compHand
 
 getHand :: String -> Hand
 getHand input
-    | input == "rock" = Rock
-    | input == "scissors" = Scissors
-    | input == "paper" = Paper
+    | input == "r" = Rock
+    | input == "s" = Scissors
+    | input == "p" = Paper
 
 writeToDB :: Hand -> IO ()
 writeToDB hand
@@ -51,34 +50,53 @@ superCache :: Cache -> Char -> Cache
 superCache (c0 : c1 : [_]) nc = (Just nc) : c0 : [c1]
 superCache _ _ = undefined
 
+getBeatingHand :: Hand -> Hand
+getBeatingHand hand
+    | hand == Rock = Paper
+    | hand == Paper = Scissors
+    | hand == Scissors = Rock
+
 groupIt :: String -> String -> Map Char Int -> Map Char Int
-groupIt key content accMap
-    | key `isInfixOf` content = groupIt key (tail content) (insertWith (+) (content !! 3) 1 accMap)
+
+-- TODO: game beggining
+groupIt key content  accMap
+    | length content >= 4 && key `isInfixOf` content = groupIt key (tail content) (insertWith (+) (content !! 3) 1 accMap)
+    | length content < 4 = accMap
+    | length key < 3 = accMap
     | otherwise = groupIt key (tail content) accMap
 
--- checkIt :: Map Char Int -> Hand
--- checkIt superMap = 
---    foldlWithKey f 
-
+checkIt :: Map Char Int -> IO Hand
+checkIt superMap = do
+    go Nothing $ toList superMap
+    where
+        go :: Maybe (Char, Int) -> [(Char, Int)] -> IO Hand
+        go _ [] = runRVar hand DevRandom
+        go Nothing [(c, _)] = return $ getBeatingHand $ getHand [c]
+        go (Just (c0, n0)) [(c1, n1)]
+            | n0 > n1 = go Nothing [(c0, n0)]
+            | otherwise = go Nothing [(c1, n1)]
+        go (Just (c0, n0)) ((c1, n1) : cns)
+            | n0 > n1 = go (Just (c0, n0)) cns
+            | otherwise = go (Just (c1, n1)) cns
+        go Nothing ((c, n) : cns) = go (Just (c, n)) cns
 
 calculateCompHand :: Cache -> IO Hand
 calculateCompHand cs = do
     content <- readFile "db"
-    undefined
+    print content
+    checkIt $ groupIt stringCache content empty
         where
-        sum = catMaybes cs
--- runRVar hand DevRandom
--- calculateCompHand (Just a, _, _) = do
---  content <- readFile "db"
---  undefined
-
+        stringCache = catMaybes cs
 
 play :: Cache -> IO ()
 play cache = do
     putStrLn "Choose rock, paper or scissors"
     userHand <- getLine
-    compHand <- runRVar hand DevRandom
+    print $ "1"
+    compHand <- calculateCompHand cache
+    print $ "2"
     writeToDB $ getHand userHand
+    print $ "3"
     print $ compHand
     print $ getHand userHand `playAgainst` compHand
     play $ superCache cache $ head userHand
