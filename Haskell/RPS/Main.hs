d9ac32b7326d0d22c0ed70595b64235494cc56c1
Piotr Lewandowski <piotr.lewandowski@teradata.com>
2016-04-12 15:46:56 +0200

    working alpha version

--- Haskell/RPS/Main.hs
+++ Haskell/RPS/Main.hs
@@ -22,8 +22,9 @@ possibleHands = [Rock, Paper, Scissors]
 hand :: RVar Hand
 hand = choice(possibleHands)
 
-computerHand :: IO Hand
-computerHand = runRVar hand DevRandom
+randomHand :: IO Hand
+randomHand = runRVar hand DevRandom
+
 
 playAgainst :: Hand -> Hand -> String
 playAgainst userHand compHand
@@ -31,24 +32,25 @@ playAgainst userHand compHand
     | userHand == compHand = "Draw!"
     | otherwise = "You have lost!"
 
-getHand :: String -> Hand
+getHand :: Char -> Hand
 getHand input
-    | input == "r" = Rock
-    | input == "s" = Scissors
-    | input == "p" = Paper
+    | input == 'r' = Rock
+    | input == 's' = Scissors
+    | input == 'p' = Paper
 
-writeToDB :: Hand -> IO ()
-writeToDB hand
+writeHandToDB :: Hand -> IO ()
+writeHandToDB hand
    | hand == Rock = writeToDefaultDB "r"
    | hand == Paper = writeToDefaultDB "p"
    | hand == Scissors = writeToDefaultDB "s"
     where
      writeToDefaultDB :: String -> IO ()
      writeToDefaultDB = appendFile "db"
+
 type Cache = [Maybe Char]
-superCache :: Cache -> Char -> Cache
-superCache (c0 : c1 : [_]) nc = (Just nc) : c0 : [c1]
-superCache _ _ = undefined
+lastThreePlays :: Cache -> Char -> Cache
+lastThreePlays (c0 : c1 : [_]) nc = (Just nc) : c0 : [c1]
+lastThreePlays _ _ = [Nothing, Nothing, Nothing] -- empty cache
 
 getBeatingHand :: Hand -> Hand
 getBeatingHand hand
@@ -62,7 +64,7 @@ groupIt :: String -> String -> Map Char Int -> Map Char Int
 groupIt key content  accMap
     | contentLength < 2 = accMap
     | keyLength >= contentLength = accMap
-    | contentLength > keyLength  && (reverse key) `isInfixOf` content = groupIt key (tail content) (insertWith (+) (content !! (1 + keyLength)) 1 accMap)
+    | (reverse key) `isPrefixOf` content = groupIt key (tail content) (insertWith (+) (content !! keyLength) 1 accMap)
     | otherwise = groupIt key (tail content) accMap
 	where
 	 keyLength = length key
@@ -73,8 +75,8 @@ checkIt superMap = do
     go Nothing $ toList superMap
     where
         go :: Maybe (Char, Int) -> [(Char, Int)] -> IO Hand
-        go _ [] = runRVar hand DevRandom
-        go Nothing [(c, _)] = return $ getBeatingHand $ getHand [c]
+        go _ [] = randomHand
+        go Nothing [(c, _)] = return $ getBeatingHand $ getHand c
         go (Just (c0, n0)) [(c1, n1)]
             | n0 > n1 = go Nothing [(c0, n0)]
             | otherwise = go Nothing [(c1, n1)]
@@ -86,19 +88,19 @@ checkIt superMap = do
 calculateCompHand :: Cache -> IO Hand
 calculateCompHand cs = do
     content <- readFile "db"
-    checkIt $ groupIt stringCache content empty
+    checkIt $ groupIt cacheValue content empty
         where
-        stringCache = catMaybes cs
+        cacheValue = catMaybes cs
 
 play :: Cache -> IO ()
 play cache = do
-    putStrLn "Choose rock, paper or scissors"
-    userHand <- getLine
     compHand <- calculateCompHand cache
-    writeToDB $ getHand userHand
+    putStrLn "Choose (r)ock, (p)aper or (s)cissors"
+    userHand <- getLine
+    writeHandToDB $ getHand $ head userHand
     print $ compHand
-    print $ getHand userHand `playAgainst` compHand
-    play $ superCache cache $ head userHand
+    print $ getHand (head userHand) `playAgainst` compHand
+    play $ lastThreePlays cache  $ head userHand
 
 main :: IO ()
 main = play [Nothing, Nothing, Nothing]
