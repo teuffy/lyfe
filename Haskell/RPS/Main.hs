5ba15f808e52fe26e41371414249fcf68afebb63
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2016-04-12 19:33:26 +0200

    slight refactor

--- Haskell/RPS/Main.hs
+++ Haskell/RPS/Main.hs
@@ -15,15 +15,13 @@ instance Ord Hand where
     (>) Scissors Paper = True
     (>) Paper Rock = True
     (>) _ _ = False
-
 possibleHands :: [Hand]
 possibleHands = [Rock, Paper, Scissors]
 
-hand :: RVar Hand
-hand = choice(possibleHands)
-
 randomHand :: IO Hand
-randomHand = runRVar hand DevRandom
+randomHand = runRVar handChoice DevRandom
+    where
+        handChoice = choice(possibleHands)
 
 playAgainst :: Hand -> Hand -> String
 playAgainst userHand compHand
@@ -31,8 +29,8 @@ playAgainst userHand compHand
     | userHand == compHand = "Draw!"
     | otherwise = "You have lost!"
 
-getHand :: Char -> Hand
-getHand input
+getHandFromChar :: Char -> Hand
+getHandFromChar input
     | input == 'r' = Rock
     | input == 's' = Scissors
     | input == 'p' = Paper
@@ -52,19 +50,24 @@ updateLastThreePlays (c0 : c1 : [_]) nc = (Just nc) : c0 : [c1]
 updateLastThreePlays _ _ = [Nothing, Nothing, Nothing] -- empty cache
 
 getBeatingHand :: Hand -> Hand
-getBeatingHand hand
-    | hand == Rock = Paper
-    | hand == Paper = Scissors
-    | hand == Scissors = Rock
+getBeatingHand inHand
+    | inHand == Rock = Paper
+    | inHand == Paper = Scissors
+    | inHand == Scissors = Rock
+    | otherwise = undefined
 
-groupIt :: String -> String -> Map Char Int -> Map Char Int
-groupIt key content  accMap
-    | contentLength < 2 || keyLength >= contentLength = accMap
-    | (reverse key) `isPrefixOf` content = groupIt key (tail content) (insertWith (+) (content !! keyLength) 1 accMap)
-    | otherwise = groupIt key (tail content) accMap
-	where
-	 keyLength = length key
-         contentLength = length content
+groupIt :: String -> String -> Map Char Int
+groupIt cache databaseContent =
+    go cache databaseContent empty
+    where
+        go :: String -> String -> Map Char Int -> Map Char Int
+        go key content accMap
+            | contentLength < 2 || keyLength >= contentLength = accMap
+            | (reverse key) `isPrefixOf` content = go key (tail content) (insertWith (+) (content !! keyLength) 1 accMap)
+            | otherwise = go key (tail content) accMap
+            where
+                keyLength = length key
+                contentLength = length content
 
 checkIt :: Map Char Int -> IO Hand
 checkIt superMap = do
@@ -72,19 +75,29 @@ checkIt superMap = do
     where
         go :: Maybe (Char, Int) -> [(Char, Int)] -> IO Hand
         go _ [] = randomHand
-        go Nothing [(c, _)] = return $ getBeatingHand $ getHand c
+        go Nothing [(c, _)] = return $ getBeatingHand $ getHandFromChar c
         go (Just (c0, n0)) [(c1, n1)]
             | n0 > n1 = go Nothing [(c0, n0)]
-            | otherwise = go Nothing [(c1, n1)]
+            | n0 < n1 = go Nothing [(c1, n1)]
+            | otherwise = do
+                (c, n) <- runRVar equivalentHands DevRandom
+                go Nothing [(c, n)]
+                    where
+                        equivalentHands = choice([(c0, n0), (c1, n1)])
         go (Just (c0, n0)) ((c1, n1) : cns)
             | n0 > n1 = go (Just (c0, n0)) cns
-            | otherwise = go (Just (c1, n1)) cns
+            | n0 < n1 = go (Just (c1, n1)) cns
+            | otherwise =  do
+                (c, n) <- runRVar equivalentHands DevRandom
+                go (Just (c, n)) cns
+                    where
+                        equivalentHands = choice([(c0, n0), (c1, n1)])
         go Nothing ((c, n) : cns) = go (Just (c, n)) cns
 
 calculateCompHand :: Cache -> IO Hand
 calculateCompHand cs = do
     content <- readFile "db"
-    checkIt $ groupIt cacheValue content empty
+    checkIt $ groupIt cacheValue content
         where
         cacheValue = catMaybes cs
 
@@ -93,9 +106,9 @@ play cache = do
     compHand <- calculateCompHand cache
     putStrLn "Choose (r)ock, (p)aper or (s)cissors"
     userHand <- getLine
-    writeHandToDB $ getHand $ head userHand
+    writeHandToDB $ getHandFromChar $ head userHand
     print $ compHand
-    print $ getHand (head userHand) `playAgainst` compHand
+    print $ getHandFromChar (head userHand) `playAgainst` compHand
     play $ updateLastThreePlays cache  $ head userHand
 
 main :: IO ()
