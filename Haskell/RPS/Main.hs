d60bec0f66a03b81f56032ada328e7de1ab2a6f5
Piotr Lewandowski <piotr.lewandowski@teradata.com>
2016-04-14 10:01:00 +0200

    refactoring!

--- Haskell/RPS/Main.hs
+++ Haskell/RPS/Main.hs
@@ -9,12 +9,8 @@ import           Data.Random.Extras
 import           Data.Random.Source.DevRandom
 import           Data.RVar
 
-data Hand = Rock | Scissors | Paper deriving (Eq, Show)
-instance Ord Hand where
-    (>) Rock Scissors = True
-    (>) Scissors Paper = True
-    (>) Paper Rock = True
-    (>) _ _ = False
+data Hand = Rock | Scissors | Paper deriving (Ord, Eq, Show)
+
 possibleHands :: [Hand]
 possibleHands = [Rock, Paper, Scissors]
 
@@ -24,25 +20,45 @@ randomHand = runRVar handChoice DevRandom
         handChoice = choice(possibleHands)
 
 playAgainst :: Hand -> Hand -> String
-playAgainst userHand compHand
-    | userHand > compHand = "You have won"
-    | userHand == compHand = "Draw!"
+playAgainst userHand compHand 
+    | playAgainst' userHand compHand == 1 = "You have won"
+    | playAgainst' userHand compHand == 0 = "Draw!"
     | otherwise = "You have lost!"
 
-getHandFromChar :: Char -> Hand
-getHandFromChar input
-    | input == 'r' = Rock
-    | input == 's' = Scissors
-    | input == 'p' = Paper
+playAgainst' :: Hand -> Hand -> Int
+playAgainst' hand otherHand
+    | hand == otherHand = 0
+playAgainst' Rock otherHand
+    | otherHand == Paper = -1
+    | otherHand == Scissors = 1
+playAgainst' Paper otherHand
+    | otherHand == Scissors = -1
+    | otherHand == Rock = 1
+playAgainst' Scissors otherHand
+    | otherHand == Rock = -1
+    | otherHand == Paper = 1
+
+deserializeHand :: Char -> Hand
+deserializeHand inputChar
+    | inputChar == 'r' = Rock
+    | inputChar == 's' = Scissors
+    | inputChar == 'p' = Paper
+    | otherwise = error "Non-existing serialized Hand passed"
+
+serializeHand :: Hand -> Char
+serializeHand inputHand
+    | inputHand == Rock = 'r'
+    | inputHand == Scissors = 's'
+    | inputHand == Paper = 'p'
+
+getDatabaseContent :: IO String
+getDatabaseContent = readFile "db"
 
 writeHandToDB :: Hand -> IO ()
-writeHandToDB hand
-   | hand == Rock = writeToDefaultDB "r"
-   | hand == Paper = writeToDefaultDB "p"
-   | hand == Scissors = writeToDefaultDB "s"
+writeHandToDB h = writeCharToDefaultDB $ serializeHand h
     where
-        writeToDefaultDB :: String -> IO ()
-        writeToDefaultDB = appendFile "db"
+        writeCharToDefaultDB :: Char -> IO ()
+        writeCharToDefaultDB c = appendFile "db" [c]
 
 type Cache = [Maybe Char]
 updateLastThreePlays :: Cache -> Char -> Cache
@@ -56,59 +72,62 @@ getBeatingHand inHand
     | inHand == Scissors = Rock
     | otherwise = undefined
 
-groupIt :: String -> String -> Map Char Int
-groupIt cache databaseContent =
-    go cache databaseContent empty
+getHandOccurencesBasedOnCache :: String -> IO (Map Hand Int)
+getHandOccurencesBasedOnCache cache = do
+    content <- getDatabaseContent
+    return $ go cache content empty
     where
-        go :: String -> String -> Map Char Int -> Map Char Int
+        go :: String -> String -> Map Hand Int -> Map Hand Int
         go key content accMap
             | contentLength < 2 || keyLength >= contentLength = accMap
-            | (reverse key) `isPrefixOf` content = go key (tail content) (insertWith (+) (content !! keyLength) 1 accMap)
+            | (reverse key) `isPrefixOf` content = go key (tail content) (insertWith (+) (deserializeHand (content !! keyLength)) 1 accMap)
             | otherwise = go key (tail content) accMap
             where
                 keyLength = length key
                 contentLength = length content
 
-checkIt :: Map Char Int -> IO Hand
-checkIt superMap = do
-    go Nothing $ toList superMap
+chooseBestHand :: Map Hand Int -> IO Hand
+chooseBestHand nextHandOccurences = do
+    go Nothing $ toList nextHandOccurences
     where
-        go :: Maybe (Char, Int) -> [(Char, Int)] -> IO Hand
+        selectRandomHandFromEqOccurences :: Hand -> Hand -> IO Hand
+        selectRandomHandFromEqOccurences h1 h2 = runRVar (choice [h1, h2]) DevRandom   
+        go :: Maybe (Hand, Int) -> [(Hand, Int)] -> IO Hand
         go _ [] = randomHand
-        go Nothing [(c, _)] = return $ getBeatingHand $ getHandFromChar c
-        go (Just (c0, n0)) [(c1, n1)]
-            | n0 > n1 = go Nothing [(c0, n0)]
-            | n0 < n1 = go Nothing [(c1, n1)]
+        go Nothing [(h, _)] = return $ getBeatingHand h 
+        go (Just (h0, n0)) [(h1, n1)]
+            | n0 > n1 = go Nothing [(h0, n0)]
+            | n0 < n1 = go Nothing [(h1, n1)]
             | otherwise = do
-                (c, n) <- runRVar equivalentHands DevRandom
-                go Nothing [(c, n)]
-                    where
-                        equivalentHands = choice([(c0, n0), (c1, n1)])
-        go (Just (c0, n0)) ((c1, n1) : cns)
-            | n0 > n1 = go (Just (c0, n0)) cns
-            | n0 < n1 = go (Just (c1, n1)) cns
+                h <- selectRandomHandFromEqOccurences h0 h1
+                go Nothing [(h, n0)]
+        go (Just (h0, n0)) ((h1, n1) : cns)
+            | n0 > n1 = go (Just (h0, n0)) cns
+            | n0 < n1 = go (Just (h1, n1)) cns
             | otherwise =  do
-                (c, n) <- runRVar equivalentHands DevRandom
-                go (Just (c, n)) cns
-                    where
-                        equivalentHands = choice([(c0, n0), (c1, n1)])
-        go Nothing ((c, n) : cns) = go (Just (c, n)) cns
+                h <- selectRandomHandFromEqOccurences h0 h1
+                go (Just (h, n0)) cns         
+        go Nothing ((h, n) : cns) = go (Just (h, n)) cns
+           
 
-calculateCompHand :: Cache -> IO Hand
-calculateCompHand cs = do
-    content <- readFile "db"
-    checkIt $ groupIt cacheValue content
+calculateAIHand :: Cache -> IO Hand
+calculateAIHand cs = do
+    nextHandOccurences <- getHandOccurencesBasedOnCache cacheValue
+    chooseBestHand nextHandOccurences
         where
         cacheValue = catMaybes cs
 
 play :: Cache -> IO ()
 play cache = do
-    compHand <- calculateCompHand cache
+-- calculateAIPlay
+-- getAndValidateUserHand
+-- play
+    compHand <- calculateAIHand cache
     putStrLn "Choose (r)ock, (p)aper or (s)cissors"
     userHand <- getLine
-    writeHandToDB $ getHandFromChar $ head userHand
+    writeHandToDB $ deserializeHand $ head userHand
     print $ compHand
-    print $ getHandFromChar (head userHand) `playAgainst` compHand
+    print $ deserializeHand (head userHand) `playAgainst` compHand
     play $ updateLastThreePlays cache  $ head userHand
 
 main :: IO ()
